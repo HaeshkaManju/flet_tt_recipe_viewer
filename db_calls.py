375ed100b41533bc7f86015c2edf566126c2e568
C. Michael <Haeshka@gmail.com>
2023-10-03 15:09:54 -0400

    Update db_calls.py
    1

--- db_calls.py
+++ db_calls.py
@@ -11,14 +11,20 @@ def call_types():
     return results
 
 def call_recipes():
-    rows = []
     results = session.query(Recipe).all()
     for r in results:
-        rows.append(r)
-    return rows
+        print(r)
+    return results
 
 def search_recipes_by_name(search_text):
     results = session.query(Recipe).filter(Recipe.name.like(f'%{search_text}%')).all()
     for r in results:
         print(r)
     return results
+
+def search_recipes_by_type(search_type):
+    results = session.query(Recipe).filter(Recipe.type_id == search_type).all()
+    for r in results:
+        print(r)
+    return results
+
